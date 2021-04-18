b07d893f24767be40cc1734e940f402b513e8dd2
Bryan Law <bryanwal6@gmail.com>
2021-04-18 04:58:15 -0400

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import logo from './RutgersVax.png'
 import React, {useState, useEffect} from 'react';
-import { Button, Form, FormGroup, Label, Input, ListGroupItem, ListGroup, Row } from 'reactstrap';
+import validator from 'validator';
+import { Button, Form, FormGroup, Label, Input, Jumbotron, Fade, Row } from 'reactstrap';
 const axios = require("axios")
 
 function App() {
   const [cities, setcities] = useState([])
   const [selection, setselection] = useState([])
+  const [error, seterror] = useState('')
+
+  const [fadeIn, setFadeIn] = useState(false);
+
+  const toggle = () => setFadeIn(!fadeIn);
   
-  var selectedCity = "ABSECON"
   var phoneNumber = ""
 
   useEffect(()=>{
@@ -28,10 +34,6 @@ function App() {
     })
   } 
 
-  const updateSelection = (city) =>{
-    selectedCity = city.target.value;
-  }
-
   const removeSelection = (toRemove) =>{
     var temp = [];
     for(var city of selection){
@@ -48,49 +50,62 @@ function App() {
 
   const addToDatabase = (e) =>{
     e.preventDefault()
-    console.log(phoneNumber)
-    console.log(selection)
 
+    if(!validator.isMobilePhone(phoneNumber, 'en-US')){
+      phoneNumber = '';
+      alert("Invalid Phone Number");
+      seterror("Invalid Phone Number");
+      return;
+    }
+
+    console.log(phoneNumber)
     axios.post(`/newnumber`,{
       phone: phoneNumber,
       cities: selection
     })
+    window.location.reload();
   }
 
   return (
-    <div className="App">
-      <Row>
-      <select onChange={updateSelection}>
-        {
-          cities.map((city) =>
-            <option key={city}>{city}</option>
-          )
-        }
-      </select>
-      <Button onClick={()=>{
-        if(!selection.includes(selectedCity))
-          setselection([...selection,selectedCity])}
-        } color="primary">Add</Button>{' '}
-      </Row>
-
-      <ListGroup style={{alignItems:'center'}}>
-      {
-        selection.map((number) =>
-            <Row>
-            <ListGroupItem key={number}>{number}</ListGroupItem>
-            <Button outline onClick={()=>removeSelection(number)}color="danger">X</Button>
-            </Row>
-        )
-      }
-      </ListGroup>
+    <div>
       
-      <Form inline onSubmit={addToDatabase}>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label for="number" className="mr-sm-2">Phone Number</Label>
-          <Input onChange={updatePhone} type="tel" name="number" id="number" placeholder="" />
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
+      <div className="App">
+        <Jumbotron style={{width:'100vh', marginTop:'-20%', marginBottom:'-5%'}} className="Info">
+          <Row>
+            <img className="logo" alt="logo" src={logo}/>
+            <h1 style={{marginLeft:'150px'}} className="display-3">RU VAXXED?</h1>
+          </Row>
+          <p className="lead">Select cities that you want to be notified for vaccine appointments</p>
+          <hr color="white" className="my-2" />
+          <p className="lead">You will be notified by text when an appointment becomes available</p>
+          <p className="lead">
+            <Button style={{backgroundColor:'#87cefa'}} onClick={toggle} >Subscribe</Button>
+          </p>
+        </Jumbotron>
+        <Fade in={fadeIn} tag="h5" className="mt-3">
+            
+          <FormGroup style={{textAlign:'center'}}>
+            <Label for="exampleSelectMulti">Select Cities in NJ</Label>
+            <Input style={{height:"40vh"}} type="select" name="selectMulti" id="selectMulti" multiple>
+              {
+                cities.map((city) =>
+                  selection.includes(city)?
+                  <option style={{backgroundColor:'lightgray'}} onClick={() =>{removeSelection(city)}} key={city}>{city}</option>:
+                  <option onClick={()=>{setselection([...selection,city])}} key={city}>{city}</option>
+                )
+              }
+            </Input>
+          </FormGroup>
+          
+          <Form inline onSubmit={addToDatabase}>
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+              {/* <Label style={{marginLeft:50}} for="number" className="mr-sm-2">Phone Number</Label> */}
+              <Input onChange={updatePhone} type="tel" name="number" id="number" style={error===''?{}:{border:'2px solid red'}} placeholder={error===''?"Enter Phone Number":error}/>
+            </FormGroup>  
+            <Button style={{backgroundColor:'#87cefa'}}>Submit</Button>
+          </Form>
+        </Fade>
+      </div>
     </div>
   );
 }
